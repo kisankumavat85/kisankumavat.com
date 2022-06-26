473f07a5340ebfb2e08f3d98b2276e8452dbca27
Kisan Kumavat <kisankumavat85@gmail.com>
2022-06-26 14:36:21 +0530

    JavaScript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ const Home = (props) => {
             <br />
             I&apos;m <span>Kisan</span>
             <br />
-            Software Developer
+            JavaScript Developer
           </h1>
           <div className="img-div">
             <Image
